d9aa1a715db6defe66f13a81d20cd9297d844655
jumpingaccess <laurent@jumpingaccess.com>
2017-04-22 10:12:46 +0200

    Add files via upload
    Added:Log into File
    Fix : Reception from Equipe manual buttons.

--- main.js
+++ main.js
@@ -6,6 +6,54 @@ const TickTimer = require("./time-ticks.js");
 var Portfind;
 var SerialPort = require('serialport');
 var net = require('net');
+var fs = require('fs');
+var dateFormat = require('dateformat');
+var now = new Date();
+var dateoftheday = dateFormat(now,"ddmmyy");
+
+var payload_output = {
+      time: null,
+      previousTime: null,
+      running: false,
+      countDown: false,
+      countDownDiff: null,
+      countDownValue: null,
+      waiting: false,
+      faults: null,
+      totalFaults: null,
+      fenceFaults: null,
+      timeFaults: null,
+      timeAdded: null,
+      id: null,
+      startNo: null,
+      rider: null,
+      horse: null,
+      round: 1,
+      phase: 1,
+      timeToBeatDiff: null,
+      timeToBeatTime: null,
+      timekeepingOutputId: null
+};
+
+
+////////////////////////////////////////////////////////
+///// File system verification for logs of activity
+////////////////////////////////////////////////////////
+var path_log = "./logs";
+if (!fs.existsSync(path_log)){
+    fs.mkdirSync(path_log);
+    console.log("LOG Folder :"+path_log+" has been successfully created");
+} else {
+    console.log("LOG Folder :"+path_log+" already exists");
+}
+//////////////////////////////////////////////////////////
+///// Creation of the Log File
+//////////////////////////////////////////////////////////
+
+var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Start of the log file\r\n";
+fs.writeFile(path_log+"/log"+dateoftheday+".txt",ligne)
+
+
 
 ////////////////////////////////////////////////////////
 // WebSocket Server to Equipe 
@@ -14,7 +62,9 @@ var net = require('net');
 const wss = new WebSocket.Server({port: 21000});
 CLIENTS=[];
 var json_com="";
-
+var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Start of the Websocket Server \r\n";
+fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
+ 
 var app = express();
 
 wss.on('connection', connection);   
@@ -29,7 +79,10 @@ function connection(ws) {
             ws.send(json_txt,function (){ /* ignore errors */ })    ;
         
         }, 75);
-    
+
+        //var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] New Connection from "+ws._socket.remoteAddress+":"+ ws._socket.remotePort+" \r\n";
+        //fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
+
     
         ///////////////////////////////////////
         //// Json Parser 
@@ -40,55 +93,95 @@ function connection(ws) {
         function sendAll (message) {
             for (var i=0; i<CLIENTS.length; i++) {
                 CLIENTS[i].send(message);
-            }
-        }
-        function wsdata(data) {
-        /*Received:{"type":"cmd","payload":{"cmd":"trigger","node":"1"}}
-        Received:{"type":"cmd","payload":{"cmd":"trigger","node":"2"}}
-        Received:{"type":"cmd","payload":{"cmd":"trigger","node":"4"}}
-        */
-
-        var msg_received = JSON.parse(data);
-        var msg_type = msg_received["type"];
-        var msg_cmd = msg_received["payload"]["cmd"];
-        var msg_node = msg_received["payload"]["node"];
-        //console.log("parsed:"+msg_type +" / "+msg_cmd+" / "+msg_node);
-        
-        if ((msg_type=='cmd') && (msg_cmd=="trigger") && (msg_node != "") ) {
-           //console.log(`Received:`+data);
-            var rep = JSON.stringify({"type": "pulse", "payload":{ "timeTicks": TickTimer.now(), "node": msg_node, "resends": 1, "batteryLevel": 4, "RSSI": -10}});
-            //{ "type": "pulse", "payload": { "timeTicks": 60926928, "node": "1", "resends": "3", "batteryLevel": "2", "RSSI": "79" }}
-        
-            //wss.clients.forEach(function each(client) {
-                
-                //if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    //console.log(client+" / "+rep);
-                    sendAll(rep);
-                    //client.send(rep, function ack(error) {
-                        // If error is not defined, the send has been completed, otherwise the error 
-                        // object will indicate what failed. 
-                            //console.log(error);
-                   // });
-              //  }
-        
-            //});
-            
-            /*
-            wss.broadcast = function broadcast(rep) {
-                 wss.clients.forEach(function each(client) {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(rep);
-                    }
-                });    
             };
-            */
-
-
-           // console.log(rep);
-        } else {
-            //console.log(`Received:`+data);
-
         };
+        function wsdata(data) {
+            var msg_received = JSON.parse(data);
+            var msg_type = msg_received["type"];
+            var msg_cmd = msg_received["payload"]["cmd"];
+            var msg_node = msg_received["payload"]["node"];
+            if ((msg_type=='cmd') && (msg_cmd=="trigger") && (msg_node != "") ) {
+                /////////////////////////////////////////////////////////////////////////////
+                //// Send Pulse information when requested ( Tag Pulse or Manual in Equipe)
+                /////////////////////////////////////////////////////////////////////////////
+                var rep = JSON.stringify({"type": "pulse", "payload":{ "timeTicks": TickTimer.now(), "node": msg_node, "resends": 1, "batteryLevel": 4, "RSSI": -10}});
+                sendAll(rep);
+                var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] New Pulse information on node:"+msg_node+" \r\n";
+                fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+            };
+
+            if ((msg_type=='output')) {
+                /////////////////////////////////////////////////////////////////////////////
+                ///// Payload Value treatment
+                /////////////////////////////////////////////////////////////////////////////
+
+                payload_output.countDown = msg_received["payload"]["countDown"];
+                payload_output.countDownDiff = msg_received["payload"]["countDownDiff"];
+                payload_output.countDownValue = msg_received["payload"]["countDownValue"];
+                payload_output.faults = msg_received["payload"]["faults"];
+                payload_output.fenceFaults = msg_received["payload"]["fenceFaults"];
+                payload_output.horse=msg_received["payload"]["horse"];
+                payload_output.id=msg_received["payload"]["id"];
+                payload_output.phase=msg_received["payload"]["phase"];
+                payload_output.previousTime=msg_received["payload"]["previousTime"];
+                payload_output.rider=msg_received["payload"]["rider"];
+                payload_output.round=msg_received["payload"]["round"];
+                payload_output.running=msg_received["payload"]["running"];
+                payload_output.startNo=msg_received["payload"]["startNo"];
+                payload_output.time=msg_received["payload"]["time"];
+                payload_output.timeAdded=msg_received["payload"]["timeAdded"];
+                payload_output.timeFaults=msg_received["payload"]["timeFaults"];
+                payload_output.timeToBeatDiff=msg_received["payload"]["timeToBeatDiff"];
+                payload_output.timeToBeatTime=msg_received["payload"]["timeToBeatTime"];
+                payload_output.timekeepingOutputId=msg_received["payload"]["timekeepingOutpuId"];
+                payload_output.totalFaults=msg_received["payload"]["totalFaults"];
+                payload_output.waiting=msg_received["payload"]["waiting"];
+                var saverider, savetime, countdiff, phase1,ret,elim;
+
+                if((payload_output.running== false)&&(payload_output.countDown==true)){
+                    if (saverider != payload_output.rider) {
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] New Start: Rider"+payload_output.rider+" with horse: "+payload_output.horse+" \r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                    };
+                };
+                if((payload_output.running== true)&&(payload_output.countDown==false)&&(payload_output.phase==1)){
+                    if (countdiff != payload_output.countDownDiff) {
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] End of CountDown at: "+payload_output.countDownDiff+" \r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                    };
+                };
+
+                if((payload_output.running== true)&&(payload_output.countDown==false)&&(payload_output.phase==2)){
+                    if (phase1 != payload_output.previousTime) {
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Phase 1 Time: "+payload_output.previousTime+" \r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                    };
+                };
+
+                if((payload_output.running== false)&&(payload_output.countDown==false)){
+                    if (savetime != payload_output.time) {
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Finish for Rider"+payload_output.rider+" with horse: "+payload_output.horse+" - Time:"+payload_output.time+" / Pen:"+payload_output.faults+" \r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                    };
+                };
+
+                if((payload_output.faults== "666")){
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"]  Retires for Rider"+payload_output.rider+" with horse: "+payload_output.horse+"\r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                };
+                if((payload_output.faults== "999")){
+                        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"]  Elimination for Rider"+payload_output.rider+" with horse: "+payload_output.horse+"\r\n";
+                        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne);
+                };
+
+                saverider = payload_output.rider;
+                savetime = payload_output.time;
+                countdiff = payload_output.countDownDiff;
+                phase1 = payload_output.phase;
+                ret=payload_output.faults;
+                elim=payload_output.faults;
+
+            };
         
         };
     
@@ -102,6 +195,9 @@ function connection(ws) {
 function wsclose() {
     clearInterval(interval);
     //console.log("Connection WS closed")
+    var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] End of the Websocket Server.\r\n";
+    fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
+
 }
  
 
@@ -112,22 +208,25 @@ function wsclose() {
 /////////////////////////////////////////////////////
 console.log ("Serial port Listing");
 console.log(' Port: ETH');
+var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Listing of the availables ports:\r\nETH\r\n";
+fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
+
 SerialPort.list(function (err, ports) {
     ports.forEach(function(port) {
         Portfind=port.comName; 
         console.log(' Port:' +port.comName);
-    });
+        var ligne = "Port:"+port.comName+"\r\n";
+        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
 
-    
+   });
         
     // Prompt command line
 
     var Portselected = readlineSync.question('Please Choose the Serial Port; ');
-
-
-
-
     if (S(Portselected).contains("COM") == true) {
+        var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Selected Port :"+Portselected+"\r\n";
+        fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
+
         ///////////////////////
         /// COM Port Signal
         ///////////////////////
@@ -146,47 +245,41 @@ SerialPort.list(function (err, ports) {
 
         function showPortOpen() {
             console.log('port open. Data rate: ' + myPort.options.baudRate);
-        }
+        };
         
         function sendSerialData(data) {
-            //console.log("received from Tag:"+data);
             var info_com = data.split(" ");
             var new_com = "";
-            //console.log(info_com);
             var j=0;
             for (var i=0; i < info_com.length; i++){
-            if (info_com[i] != "") { 
-                // console.log(j+" : "+info_com[i] + " / ");
-                    if (j == 2) {
-                        var node = info_com[i];
-                        if (S(node).contains("M") == true) {
-                            node = S(node).right(1).s;
-                        } 
-                        
-                    }
+                if (info_com[i] != "") { 
+                        if (j == 2) {
+                            var node = info_com[i];
+                            if (S(node).contains("M") == true) {
+                                node = S(node).right(1).s;
+                            };
+                            
+                        };
 
-                    j++;
-            }
+                        j++;
+                };
 
-            }
+            };
             json_com = JSON.stringify({"type":"cmd","payload":{"cmd":"trigger","node":node}});
-            //connection.apply(json_com);
-            //console.log(new_com);  
             ws_client = new WebSocket("ws://localhost:21000");
             ws_client.on('open', function open() {
                 ws_client.send(json_com);
-                //console.log(json_com);
             });
 
-        }
+        };
         
         function showPortClose() {
             console.log('port closed.');
-        }
+        };
         
         function showError(error) {
             console.log('Serial port error: ' + error);
-        }
+        };
 
     } else {
         ////////////////////////////////
@@ -198,47 +291,47 @@ SerialPort.list(function (err, ports) {
         var PORT = 7000;
         var client = new net.Socket();
         client.connect(PORT, HOST, function() {
-            console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+            console.log('Connected on TAG HEUER 545/540 to: ' + HOST + ':' + PORT);
+            var ligne = "["+TickTimer.ticksToTime(TickTimer.now())+"] Connected on TAG HEUER by network to :"+ HOST + ":" + PORT+"\r\n";
+            fs.appendFile(path_log+"/log"+dateoftheday+".txt",ligne)
         });
         client.on('data', getethdata);
-
+      
         function getethdata(data) {
-            //////////////////////////////////////////////////////////
-            /// ETH Data Treatment
-            //////////////////////////////////////////////////////////
-            var info_eth = S(data).splitLeft(' ');
-            var new_eth = "";
-            //console.log(info_com);
-            var j=0;
-            for (var i=0; i < S(data).length; i++){
-            if (info_eth[i] != "") { 
-                // console.log(j+" : "+info_com[i] + " / ");
-                    if (j == 2) {
-                        var node = info_eth[i];
-                        if (S(node).contains("M") == true) {
-                            node = S(node).right(1).s;
-                        } 
-                        
-                    }
+            var buf = new Buffer(data,"utf8");
+            if (S(buf.toString()).contains("TN") == true) {
+                
+                //////////////////////////////////////////////////////////
+                /// ETH Data Treatment
+                //////////////////////////////////////////////////////////
+                var info_eth = S(data).splitLeft(' ');
+                var new_eth = "";
+                var j=0;
+                for (var i=0; i < S(data).length; i++){
+                    if (info_eth[i] != "") { 
+                            if (j == 2) {
+                                var node = info_eth[i];
+                                if (S(node).contains("M") == true) {
+                                    node = S(node).right(1).s;
+                                }; 
+                                
+                            };
 
-                    j++;
-            }
+                            j++;
+                    };
 
-            }
-            json_com = JSON.stringify({"type":"cmd","payload":{"cmd":"trigger","node":node}});
-            //connection.apply(json_com);
-            //console.log(new_com);  
-            ws_client = new WebSocket("ws://localhost:21000");
-            ws_client.on('open', function open() {
-                ws_client.send(json_com);
-                //console.log(json_com);
-            });
+                };
+                json_com = JSON.stringify({"type":"cmd","payload":{"cmd":"trigger","node":node}});
+                ws_client = new WebSocket("ws://localhost:21000");
+                ws_client.on('open', function open() {
+                    ws_client.send(json_com);
+                });
+            };
         };
         
         client.on('close', function() {
             console.log('Connection closed');
-            //client.destroy();
         });
         
-    }
+    };
 });
